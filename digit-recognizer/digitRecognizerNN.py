f941f4b11f37aa1fc4774f6ae2ed13c7a29b3894
Alan Nguyen <alan@mirk.co>
2018-02-13 09:44:09 +1100

    Adding data-visualisation work

---
+++ digit-recognizer/digitRecognizerNN.py
@@ -0,0 +1,34 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt, matplotlib.image as mpimg
+from sklearn.model_selection import train_test_split
+
+from sklearn.neural_network import MLPClassifier
+
+labeled_images = pd.read_csv('train.csv')
+
+images = labeled_images.iloc[0:40000,1:]
+labels = labeled_images.iloc[0:40000,:1]
+train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
+
+
+test_images[test_images>0]=1
+train_images[train_images>0]=1
+
+print("TRAINING ")
+clf = MLPClassifier(solver='lbfgs', alpha=1e-5, random_state=1)
+clf.fit(train_images, train_labels.values.ravel())
+print(clf.score(test_images,test_labels))
+
+print("MAKING PREDICTIONS")
+test_data=pd.read_csv('test.csv')
+test_data[test_data>0]=1
+results=clf.predict(test_data[0:28000])
+
+df = pd.DataFrame(results)
+df.index+=1
+df.index.name='ImageId'
+df.columns=['Label']
+df.to_csv('results.csv', header=True)# -*- coding: utf-8 -*-
+
